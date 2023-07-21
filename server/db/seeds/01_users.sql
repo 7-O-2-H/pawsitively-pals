06e8d4168ae33f35f1a807936d53bf763a3cbdc9
Josh Kelly <jkelly85@alumni.uwo.ca>
2023-07-21 17:51:44 -0400

    initial server commit

---
+++ server/db/seeds/01_users.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (user_name, email, password, admin)
+VALUES ('Kathy Samuel', 'katesamuel@gmail.com', 'test', TRUE),
+('Josh Kelly', 'jkelly85@alumni.uwo.ca', 'test', TRUE),
+('test user', 't@est.com', 'user', FALSE);
