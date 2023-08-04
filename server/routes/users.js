755d7ec30199fda5051a6029226b9abd1e12f6f2
Josh Kelly <jkelly85@alumni.uwo.ca>
2023-08-04 14:48:50 -0400

    initial routes

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-const users = ['Bob', 'Alex', 'Will', 'Tristan'];
-
 router.get('/', function(req, res) {
   res.json(users);
 });
