8d4c02b6314d2bdcd5711641807686ba08a41e10
Josh Kelly <jkelly85@alumni.uwo.ca>
2023-08-05 10:31:10 -0700

    user queries

--- server/db/queries/users.js
+++ server/db/queries/users.js
@@ -7,4 +7,31 @@ const getAllUsers = () => {
 	});
 };
 
-module.exports = { getAllUsers };
+const getUserById = id => {
+	return db.query("SELECT * FROM users; WHERE id = $1", [id]).then(data => {
+		return data.rows;
+	});
+};
+
+const getUserByEmail = email => {
+  const userEmail = email;
+  return db.query(`SELECT * FROM users WHERE users.email = $1`, [userEmail])
+  .then(data => {
+    return data.rows;
+  });
+};
+
+const addUser = (user) => {
+  const values = [user.first_name, user.last_name, user.email, user.password, FALSE];
+  return pool
+    .query(`INSERT INTO users (user_name, email, password, admin) VALUES ($1, $2, $3, $4, $5) RETURNING *;`, values)
+    .then((result) => {
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log('add user error;', err.message);
+      return null;
+    });
+};
+
+module.exports = { getAllUsers, getUserById, getUserByEmail, addUser };
