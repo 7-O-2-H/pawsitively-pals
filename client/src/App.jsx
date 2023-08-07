1b6acd7b4d6d8d0fc62c1a1f0c0743d8a9166d97
Josh Kelly <jkelly85@alumni.uwo.ca>
2023-08-07 13:35:41 -0700

    Navbar links, init css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,8 @@ import axios from 'axios';
 import { useState, useEffect } from 'react'
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import './App.css';
+import Navbar from './components/Navbar';
+//import './App.css';
 
 export default function App() {
 
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import './styles/Navbar.css'
+
+export default function Navbar() {
+
+  return (
+
+    <div className="navbar" >
+      <div className="logo">
+        <Link to='/'>
+          pawsitively
+          PALS
+        </Link>
+      </div>
+      <div className="links">
+        <Link to="/reviews">TESTIMONIALS</Link>
+        <Link to="/login">LOGIN</Link>
+        <Link to="/signup">SIGNUP</Link>
+      </div>
+    </div>
+  )
+}
